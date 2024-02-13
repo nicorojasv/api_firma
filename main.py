e17e45ae5ea38061adfe109863b0bbad6824d1d9
Mayerlyn R <maye.yrs23@gmail.com>
2024-02-13 16:47:22 -0300

    maye

--- main.py
+++ main.py
@@ -25,17 +25,14 @@ def test_odoo(data: dict):
         # Authentication in Odoo
         common = ServerProxy('{}/xmlrpc/2/common'.format(url))
         uid = common.authenticate(db, username, password, {})
-        # Validación de días (5)
         validity = datetime.datetime.now() + datetime.timedelta(days=5)
-        validity_date = validity.date()
-        validity_date_str = validity_date.strftime('%Y-%m-%d')
-        print('validez', validity_date_str)
 
         if uid:
             models = ServerProxy('{}/xmlrpc/2/object'.format(url))
             roles = models.execute_kw(db, uid, password, 'sign.item.role', 'search_read', [[]], {'fields': ['id', 'name']})
             role_mapping = {role['name']: role['id'] for role in roles}
 
+            # DATA
             # Assuming 'Customer' and 'Employee' are the names of roles in your Odoo instance
             customer_role_id = role_mapping.get('Customer')
             employee_role_id = role_mapping.get('Employee')
@@ -43,23 +40,30 @@ def test_odoo(data: dict):
             # Obtener los SigningParties de los datos recibidos
             signing_parties = data.get('SigningParties')
             print('signing_parties', signing_parties)
-
             #obtener documento
             documentos = data.get('document')
             subject = data.get('subject')
             print('subject', subject)
             pages = data.get('docPages')
             print('pages', pages)
-
             # cantidad de paginas del documento
             num_pages = data.get('numberPages')
-            print('num_pages', num_pages)
+            print('num_pages', num_pages)           
+           
+            attachment = {'name': subject, 'datas': documentos, 'type': 'binary'}
+            attachment_id = models.execute_kw(db, uid, password, 'ir.attachment', 'create', [attachment])
+            print('hola')
 
-            # Obtener los nombres y direcciones de los SigningParties
-            for signing_party in signing_parties:
-                name = signing_party.get('name')
-                address = signing_party.get('address')
-                role = signing_party.get('legalName')
+            # Crear template
+            template_data = {'name': subject,
+                                'redirect_url': 'https://portal.firmatec.cl/',
+                                'attachment_id': attachment_id,
+                                'sign_item_ids': []}
+            for firmante in signing_parties:
+                # Consultar si el primer socio ya está registrado
+                name = firmante.get('name')
+                address = firmante.get('address')
+                role = firmante.get('legalName')
                 partner_data= {'name': name, 'email': address}
                 partner_id = models.execute_kw(db, uid, password, 'res.partner', 'search', [[('email', '=', partner_data['email'])]])
                 if not partner_id:
@@ -72,22 +76,9 @@ def test_odoo(data: dict):
                 else:
                     signature_field_employee = {'type_id':2,'required':True,'name': partner_data['name'], 'page': num_pages,'responsible_id': customer_role_id,'posX':0.7,'posY':0.85,'width':0.2,'height':0.1,'required':True}
                     partner_id_2 = partner_id
-
-
-            attachment = {'name': 'hola.pdf', 'datas': documentos, 'type': 'binary'}
-            attachment_id = models.execute_kw(db, uid, password, 'ir.attachment', 'create', [attachment])
-            print('attachment_id creado')
-
-            # Create template
-            template_data = {'name': 'Template PRUEBA',
-                             'redirect_url': 'https://portal.firmatec.cl/',
-                             'attachment_id': attachment_id,
-                                 'sign_item_ids': []}
-            for firmante in signing_parties:
-                for i in range(pages):
+                for i in range(num_pages):
                     print('iiii', i)
                     if firmante['posicion'] == 'primera':
-                        print('nombre ', firmante['name'])
                         template_data['sign_item_ids'].append(
                             (0, 0, {'type_id': firmante['signingOrder'], 'required': True, 'name': firmante['name'], 'page': i, 'responsible_id': customer_role_id, 'posX': 0.15, 'posY': 0.85, 'width': 0.2, 'height': 0.1, 'required': True})
                         )
@@ -97,27 +88,33 @@ def test_odoo(data: dict):
                         )
             template_id = models.execute_kw(db, uid, password, 'sign.template', 'create', [template_data])
 
-            print(template_id)
-            # Create signature request
+            # Validación de días (5)
+            validity_date = validity.date()
+            validity_date_str = validity_date.strftime('%Y-%m-%d')
+            print('validez', validity_date_str)
+
+            # Crear signature request
             request_data = {
                 'template_id': template_id,
-                'subject': f'	PRUEBA {subject}',
+                'subject': f'	TEST Firma {subject}',
                 'reference': 'Contrato',
-                'reminder': 3,
+                'reminder': 1,
                 'validity': validity_date_str,
+                # 'attachment_ids': [(6, 0, attachment_ids)],  # Utilizar todos los IDs de adjuntos
                 'request_item_ids': [
-                    (0, 0, {'partner_id': partner_id_1, 'role_id': employee_role_id ,'mail_sent_order': 1}), 
-                    (0, 0, {'partner_id': partner_id_2, 'role_id': customer_role_id , 'mail_sent_order': 2 }),
+                    (0, 0, {'partner_id': partner_id_1, 'role_id': customer_role_id, 'mail_sent_order': 1}), 
+                    (0, 0, {'partner_id': partner_id_2, 'role_id': employee_role_id, 'mail_sent_order': 2}),
                 ],
                 'message': """<html>
                                 <head>
-                                    <title>Maye</title>
+                                    <title>Mi página de ejemplo</title>
                                 </head>
                                 <body>
                                 Aquí va el contenido SGO3 / FirmaTec
                                 </body>
                             </html>""",
                 'state': 'sent' # shared, sent, signed, refused, canceled, expired
+
             }
             request_id = models.execute_kw(db, uid, password, 'sign.request', 'create', [request_data])
             print(request_id)
